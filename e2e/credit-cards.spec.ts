59f1a9cc47b293f12ac7d656d86b6e2cac641c87
kinloth <marcelo.gwerneck@gmail.com>
2026-07-18 20:50:24 -0300

    test: Fix credit cards empty state E2E test

--- e2e/credit-cards.spec.ts
+++ e2e/credit-cards.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 
+test.use({ storageState: 'e2e/setup/empty-storage-state.json' })
+
 test.describe('Credit Cards Management', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
